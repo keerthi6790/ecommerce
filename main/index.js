f178077f32df259e9e24e96fc616d0c918f672e9
unknown <balakeerthi2710@gamil.com>
2021-10-29 16:26:55 +0530

    first commit

---
+++ main/index.js
@@ -0,0 +1,13 @@
+function fun() {
+    alert("Your product is added to the car!")
+}
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
